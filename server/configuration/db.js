3a3d0c42e853b4b98aaa39c760fe0b6dac23e363
Itay Pinhas <itay@pinhas.org>
2018-02-22 15:18:11 +0200

    Added sequelize dependency

--- server/configuration/db.js
+++ server/configuration/db.js
@@ -1,3 +1,5 @@
 export const dbConfiguration = {
-    connection_string: "mongodb://130.61.54.23:27017"
+    connection_string: "mongodb://130.61.54.23:27017",
+    user: "safe",
+    password: "asd^hadd822J#"
 };
